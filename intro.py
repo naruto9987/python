22ff15466748b67885d23f6140d394e1ad825e4c
naruto9987 <ks3805834@gmail.com>
2025-07-20 15:48:04 +0530

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,10 @@
+#printing agreeting message
+print("hello,world!")
+
+# printing my name
+print("my name is siddhant.")
+
+# printinga fun fact about myself
+print("i love eating.")
+# performing and printing a simple calculation
+print("the result of 3+5 is:",3+5)
